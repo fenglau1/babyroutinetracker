7077e60a0c2fa814c0847ffa2fbed7e7eafad5d8
SF <fenglau1@hotmail.com>
2025-11-22 10:57:22 +0800

    Initial Commit

---
+++ Baby Routine/Baby_RoutineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Baby_RoutineApp.swift
+//  Baby Routine
+//
+//  Created by SF on 22/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct Baby_RoutineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
